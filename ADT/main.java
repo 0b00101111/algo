a6351479656b7698a41e81205389eaf7de4afc0c
Sam W <37496494+0b00101111@users.noreply.github.com>
2025-02-19 21:38:09 -0800

    Setting up InterlliJ

---
+++ ADT/main.java
@@ -0,0 +1,15 @@
+public class main {
+  public static void main(String[] args) {
+    // define a static array of size 10
+    int[] arr = new int[10];
+
+    // assign values using indices
+    arr[0] = 1;
+    arr[1] = 2;
+
+    // retrieve values using indices
+    int a = arr[0];
+
+    System.out.println(a);
+  }
+}
